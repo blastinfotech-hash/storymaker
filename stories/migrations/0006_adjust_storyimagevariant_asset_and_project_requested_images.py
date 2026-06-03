db862e5205d896ba841e50ae04353df4db356483
Blast <blastinfotech@gmail.com>
2026-06-03 16:56:04 -0300

    Correção bug 0006 migrations

--- stories/migrations/0006_adjust_storyimagevariant_asset_and_project_requested_images.py
+++ stories/migrations/0006_adjust_storyimagevariant_asset_and_project_requested_images.py
@@ -3,20 +3,19 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ("stories", "0005_alter_storyconcept_price_text_and_more"),
-    ]
+	dependencies = [
+		("stories", "0005_alter_storyconcept_price_text_and_more"),
+	]
 
-    operations = [
-        migrations.AlterField,
-        migrations.AlterField(
-            model_name="storyimagevariant",
-            name="asset",
-            field=models.FileField(blank=True, max_length=500, upload_to="stories.models.story_asset_upload_to"),
-        ),
-        migrations.AlterField(
-            model_name="storyproject",
-            name="requested_image_count",
-            field=models.PositiveSmallIntegerField(default=1),
-        ),
-    ]
+	operations = [
+		migrations.AlterField(
+			model_name="storyimagevariant",
+			name="asset",
+			field=models.FileField(blank=True, max_length=500, upload_to="stories.models.story_asset_upload_to"),
+		),
+		migrations.AlterField(
+			model_name="storyproject",
+			name="requested_image_count",
+			field=models.PositiveSmallIntegerField(default=1),
+		),
+	]
